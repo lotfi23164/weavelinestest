a76deb933c9e08013c2e2d28092e0cfdd994ae8a
lotfi23164 <71345340+lotfi23164@users.noreply.github.com>
2022-09-08 04:08:10 +0100

    setup project & user module & auth module

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { AppConfigurationModule } from './infrastructure/configuration/app-configuration.module';
+import { AppConfigurationService } from './infrastructure/configuration/app-configuration.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule,AppConfigurationModule,UsersModule, AuthModule,
+    MongooseModule.forRootAsync({
+      imports: [AppConfigurationModule],
+      inject: [AppConfigurationService],
+      useFactory: (appConfigService: AppConfigurationService) => {
+        const options: MongooseModuleOptions = {
+          uri: appConfigService.connectionString,
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        };
+        return options;
+      },
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
