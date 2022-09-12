2ee3144b0d5a3f64885e099a79a410f1152f4ff3
lotfi23164 <71345340+lotfi23164@users.noreply.github.com>
2022-09-12 03:37:33 +0100

    add: swagger module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from "./app.service"
 import { AuthModule } from "./auth/auth.module"
 import { UsersModule } from "./users/users.module"
 import { TodosModule } from './todos/todos.module';
+import 'dotenv/config';
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import * as session from "express-session"
 import * as passport from "passport"
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.use(
@@ -14,6 +16,14 @@ async function bootstrap() {
   app.use(passport.initialize())
   app.use(passport.session())
 
-  await app.listen(3000)
+  const options = new DocumentBuilder()
+  .setTitle('Conduit Blog API')
+  .setDescription('Conduit blog api')
+  .setVersion('4.5.0')
+  .build();
+const document = SwaggerModule.createDocument(app, options);
+SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap()
